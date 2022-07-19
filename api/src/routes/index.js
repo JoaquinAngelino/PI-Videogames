b01b65264df2b62e9fbffe06412b52d76ed2f44a
JoaquinAngelino <joacoangelino@gmail.com>
2022-07-19 05:54:53 -0300

    routes almost finished

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,35 +1,59 @@
 const { Router } = require('express');
-const {Videogame, Genre} = require("../db")
+const { Videogame, Genre } = require("../db")
 const axios = require("axios");
-const { findById } = require('../tools');
+const { findById, getAllGames, findByName, loadGenres, postGame } = require('../tools');
+const apiKey = process.env.API_KEY
 
 const router = Router();
 
 // ------------------------------------
-router.get('/videogames/:idVideogame', (req, res)=>{
-  const {idVideogame} = req.params
-  const finded = findById(idVideogame)
-
-  return res.send("No id given")
+// ------------------------------------
+router.get('/videogame/:idVideogame', async (req, res) => {
+  const { idVideogame } = req.params
+  const finded = await findById(idVideogame)
+  return finded ? res.json(finded) : res.json(null)
 })
 
 // ------------------------------------
-router.get('/videogames', (req, res)=>{
-  const {name} = req.query
-  if(!name){
-    return res.send("No query given")
+// ------------------------------------
+router.get('/videogames', async (req, res) => {
+  const { name } = req.query
+
+  if (!name) {
+    const allGames = await getAllGames()
+    return res.json(allGames)
   }
-})
-
-
-// ------------------------------------
-router.post('/videogames', (req, res)=>{
-
+  const result = await findByName(name)
+  res.json(result)
 })
 
 // ------------------------------------
-router.get('/genres', (req, res)=>{
+// ------------------------------------
+router.post('/videogames', async (req, res) => {
+  const {
+    name,
+    image,
+    rating,
+    released,
+    platforms,
+    genres,
+    description
+  } = req.body
+  if (name && description && platforms) {
+    const created = await postGame({ name, image, rating, released, platforms, genres, description })
+    return res.send(created)
+  }
+  return res.status(400).send({ message: "Missing Name, Description or Platforms" })
+})
 
+// ------------------------------------
+// ------------------------------------
+router.get('/genres', async (req, res) => {
+  if (await Genre.count() === 0) {
+    await loadGenres()
+  }
+  const DBgenres = await Genre.findAll()
+  return res.json(DBgenres)
 })
 
 
--- api/src/tools/index.js
+++ api/src/tools/index.js
@@ -1,16 +1,179 @@
-const {Videogame, Genre} = require("../db")
+const { Videogame, Genre } = require("../db")
 const axios = require("axios");
+const apiKey = process.env.API_KEY
 
 
-const findById = async (id) =>{
-  const dbSearch = await Videogame.findByPk(id)
-  if(dbSearch !== null) {console.log("!!!!!!!!!!!", dbSearch)}
+// ------------------------------------
+// ------------------------------------
+const DbFindById = async (id) => {
+  try {
+    const dbSearch = await Videogame.findAll({
+      include: {
+        model: Genre,
+        attributes: ["name"],
+        through: { attributes: [] }
+      }
+    })
+    const dbFinded = dbSearch.filter(e => e.id === id)
+    return (dbFinded.length) ? dbFinded : null
+  } catch (err) {
+    return null
+  }
+}
 
-  const apiSearch = await axios.get(`https://api.rawg.io/api/games/${id}?key=${API_KEY}`)
-  if(apiSearch){console.log("1111111111111111",apiSearch)}
+// ------------------------------------
+// ------------------------------------
+const ApiFindById = async (id) => {
+  try {
+    const apiSearch = (await axios.get(`https://api.rawg.io/api/games/${id}?key=${apiKey}`)).data
+    const mapGenres = apiSearch.genres.map(e => { return e.name })
+    const mapPlatforms = apiSearch.platforms.map(e => { return e.platform.name })
+    return {
+      id: id,
+      name: apiSearch.name,
+      image: apiSearch.background_image,
+      rating: apiSearch.rating,
+      released: apiSearch.released,
+      platforms: mapPlatforms,
+      genres: mapGenres,
+      description: apiSearch.description,
+    }
+  } catch (err) {
+    return null
+  }
+}
+// ------------------------------------
+// ------------------------------------
+// Retorna el Elemento que matchea "id" o null
+const findById = async (id) => {
+  const db = await DbFindById(id)
+  const api = await ApiFindById(id)
+  if (!db && !api) { return null }
+  return db ? db : api
+}
+
+// ------------------------------------
+// ------------------------------------
+// Retorna todos los games de la DB y de la API
+const getAllGames = async () => {
+
+  const dbSearch = await Videogame.findAll({
+    include: {
+      model: Genre,
+      attributes: ["name"],
+      through: { attributes: [] }
+    }
+  })
+
+  let apiSearch = (await axios.get(`https://api.rawg.io/api/games?key=${apiKey}`)).data.results
+
+  apiSearch.forEach(game => {
+    game.genres = game.genres.map(genre => { return genre.name })
+    game.platforms = game.platforms.map(pf => { return pf.platform.name })
+  })
+
+  apiSearch = apiSearch.map(el => {
+    return {
+      id: el.id,
+      name: el.name,
+      image: el.background_image,
+      rating: el.rating,
+      released: el.released,
+      platforms: el.platforms,
+      genres: el.genres,
+      description: el.description,
+    }
+  })
+  return apiSearch.concat(dbSearch)
+}
+
+// ------------------------------------
+// ------------------------------------
+// REVISAR
+const findByName = async (name) => {
+
+  let dbSearch = await Videogame.findAll({
+    include: {
+      model: Genre,
+      attributes: ["name"],
+      through: { attributes: [] }
+    }
+  })
+  dbSearch = dbSearch.filter(el => el.name.toLowerCase().includes(name.toLowerCase()))
+
+  let apiSearch = (await axios.get(`https://api.rawg.io/api/games?search=${name}&key=${apiKey}`)).data.results
+  apiSearch.forEach(game => {
+    game.genres = game.genres.map(genre => { return genre.name })
+    game.platforms = game.platforms.map(pf => { return pf.platform.name })
+  })
+
+  apiSearch = apiSearch.map(el => {
+    return {
+      id: el.id,
+      name: el.name,
+      image: el.background_image,
+      rating: el.rating,
+      released: el.released,
+      platforms: el.platforms,
+      genres: el.genres,
+      description: el.description,
+    }
+  })
+
+  return apiSearch.concat(dbSearch)
+}
+
+// ------------------------------------
+// ------------------------------------
+const loadGenres = async () => {
+  const genres = (await axios.get(`https://api.rawg.io/api/genres?key=${apiKey}`)).data.results
+  const mapGenres = genres.map(genre => { return { name: genre.name } })
+  await Genre.bulkCreate(mapGenres)
+}
+
+// ------------------------------------
+// ------------------------------------
+// Retorna true si creó el game, caso contrario false
+const postGame = async (game) => {
+
+  console.log(game.name,
+    game.image,
+    game.rating,
+    game.released,
+    game.platforms,
+    game.genres,
+    game.description)
+    
+  const games = await findByName(game.name)
+  const equal = games.filter(el => el.name.toLowerCase() === game.name.toLowerCase())
+  if (equal.length) {
+    return false
+  }
+  const created = await Videogame.create({
+    name: game.name,
+    image: game.image,
+    rating: game.rating,
+    released: game.released,
+    platforms: game.platforms,
+    description: game.description,
+  })
+  if (await Genre.count() === 0) {
+    await loadGenres()
+  }
+  const DBgenres = await Genre.findAll()
+  created.addGenres(DBgenres.filter(genre => {
+    return game.genres.includes(genre.name)
+  }))
+  return true
 }
 
 
 module.exports = {
+  postGame,
+  loadGenres,
+  findByName,
+  getAllGames,
+  DbFindById,
+  ApiFindById,
   findById,
 }
